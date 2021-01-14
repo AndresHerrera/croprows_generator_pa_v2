bd9ee8749f6769db2d070c03d9b771d8b3511d1d
Andres Herrera <t763rm3n@gmail.com>
2021-01-14 18:17:07 -0500

    update script

--- croprows-qgis-plugin/PACropRows3x/compile_resources.bat
+++ croprows-qgis-plugin/PACropRows3x/compile_resources.bat
@@ -1,9 +1,9 @@
 @echo off
 echo ==============================================
 echo ========== COMPILING RESOURCES ===============
-call "C:\Program Files\QGIS 3.10\bin\o4w_env.bat"
-call "C:\Program Files\QGIS 3.10\bin\qt5_env.bat"
-call "C:\Program Files\QGIS 3.10\bin\py3_env.bat"
+call "C:\OSGeo4W64\bin\o4w_env.bat"
+call "C:\OSGeo4W64\bin\qt5_env.bat"
+call "C:\OSGeo4W64\bin\py3_env.bat"
 @echo on
-call "C:\Program Files\QGIS 3.10\apps\Python37\Scripts\pyrcc5.bat" -o resources.py resources.qrc
+call "C:\OSGeo4W64\apps\Python37\Scripts\pyrcc5.bat" -o resources.py resources.qrc
 echo ========== DONE COMPILATION ===============
